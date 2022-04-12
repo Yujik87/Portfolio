7c76ba225c4e8428ce13e2fbe2923c59c723ca02
Yujik <yujika87@gmail.com>
2022-04-12 15:19:42 +0300

    +Skill

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import style from './Main.module.css';
+import styleContainer from './../common/styles/Container.module.css';
 
 function Main() {
     return (
         <div className={style.mainBlock}>
-            <Nav />
+
+            <div className={styleContainer.container}>
+
+
+                <div className={style.text}>
+                    <span>Hi there!</span>
+                    <h1>I am Yury Khatkevich</h1>
+                    <p>Front-End Developer</p>
+                </div>
+                <div className={style.photo}></div>
+            </div>
+
+
         </div>
     );
 }
